a2da64f32d25040668091060deb09dea4926ba67
amoblin@mpb <amoblin@gmail.com>
2014-03-27 15:06:24 +0800

    generate CA and server pub/priv key pairs.

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 #!/usr/bin/env node
 
 var http = require('http');
+var https = require('https');
 var express = require('express');
+
+// ssl
+var fs = require('fs');
+var privateKey  = fs.readFileSync('server.key', 'utf8');
+var certificate = fs.readFileSync('server.cer', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
 
 var app = express()
 app.use(express.static(__dirname + '/public'))
 
 var httpServer = http.createServer(app);
 httpServer.listen(2308);
+
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443);
 
 console.log('It works');
---
+++ generateCert.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# author: amoblin <amoblin@gmail.com>
+# file name: generateCert.sh
+# create date: 2014-03-27 14:50:35
+# This file is created from ~/.marboo/source/media/file_init/default.init.sh
+# 本文件由 ~/.marboo/source/media/file_init/default.init.sh　复制而来
+
+
+domain="$1"
+
+# CA
+openssl genrsa -out customCA.key 2048
+
+openssl req -x509 -new -key customCA.key -out customCA.cer -days 730 -subj /CN="My Custom CA"
+
+# server
+openssl genrsa -out server.key 2048
+
+openssl req -new -out server.csr -key server.key -subj /CN=${domain}
+
+openssl x509 -req -in server.csr -out server.cer -CAkey customCA.key -CA customCA.cer -days 365 -CAcreateserial -CAserial serial
